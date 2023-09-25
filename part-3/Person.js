aa3bb7b97d4305f736d375482284541f4ea7f965
ross-brown <rbrown_98@yahoo.com>
2023-09-25 14:34:43 -0500

    some formatting

--- part-3/Person.js
+++ part-3/Person.js
@@ -4,7 +4,7 @@ function Person({ name, age, hobbies }) {
       <p>Learn some information about this person:</p>
       <p>Name: {name.length > 8 ? name.slice(0, 6) : name}</p>
       <p>Age: {age}</p>
-      {age > 18 ? <h3>Please go vote!</h3> : <h3>You must be 18</h3>}
+      {age >= 18 ? <h3>Please go vote!</h3> : <h3>You must be 18</h3>}
 
       <ul>
         {hobbies.map(h => <li>{h}</li>)}
